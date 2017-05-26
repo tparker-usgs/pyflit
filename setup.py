ce260d01c47221c60935a0c06a379e3363f55e36
Tom Parker <tparker@usgs.gov>
2017-05-26 13:36:31 -0800

    tweak setup

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ pyflit
 Pyflit is a simple Python HTTP downloader.
 """
 
-from setuptools import setup, find_packages
+from setuptools import setup
 import pyflit
 
 setup(
     name="pyflit",
+    version=1.0,
     author="Galeo Tian",
     description="Pyflit is a simple Python HTTP downloader.",
     license="GNU General Public License",
     url="https://github.com/galeo/pyflit",
-    packages=pyflit,
+    packages=["pyflit"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Topic :: Software Development :: Libraries",
